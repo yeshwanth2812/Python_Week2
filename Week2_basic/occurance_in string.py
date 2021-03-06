c2fb0012461da693abc4bb6c362486c428694613
yeshwanth281297 <yesh281297@gmail.com>
2021-03-06 08:39:11 +0530

    ADDED : Occurance in String

---
+++ Week2_basic/occurance_in string.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author: Yeshwanth
+# @Date: 2021-01-04 18:58:12
+# @Last Modified by: Yeshwanth
+# @Last Modified time: 2021-01-09 12:30:53
+# @Title: count the occurance in a string
+
+statement = "Was it all in my fantasy? Where are you now? Were you only imaginary? Where are you now?"
+
+print(statement.count("wh"))
+
