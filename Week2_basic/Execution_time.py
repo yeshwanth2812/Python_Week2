c41585d3d9a06d09f00b2577af455e31518a9e55
yeshwanth281297 <yesh281297@gmail.com>
2021-03-06 08:36:31 +0530

    ADDED : Execution Time

---
+++ Week2_basic/Execution_time.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Author: Yeshwanth
+# @Date: 2021-01-04 18:58:12
+# @Last Modified by: Yeshwanth
+# @Last Modified time: 2021-01-09 12:30:53
+# @Title: Execution time
+
+import time
+def sum_of_n_numbers(n):
+    start_time = time.time()
+    s = 0
+    for i in range(1,n+1):
+        s = s + i
+    end_time = time.time()
+    return s,end_time-start_time
+
+n = 5
+print("\nTime to sum of 1 to ",n," and required time to calculate is :",sum_of_n_numbers(n))
