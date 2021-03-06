9d8319e871e465622a7565c49c4b60c7157e469c
yeshwanth281297 <yesh281297@gmail.com>
2021-03-06 08:37:20 +0530

    ADDED : External Command

---
+++ Week2_basic/external_command.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: Yeshwanth
+# @Date: 2021-01-04 18:58:12
+# @Last Modified by: Yeshwanth
+# @Last Modified time: 2021-01-09 12:30:53
+# @Title: External command
+
+from subprocess import call
+call(["ls", "-l"])
