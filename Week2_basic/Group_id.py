0dcfcf4863682495849b9f6526532ed220712970
yeshwanth281297 <yesh281297@gmail.com>
2021-03-06 08:32:34 +0530

    ADDED: Group id

---
+++ Week2_basic/Group_id.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author: Yeshwanth
+# @Date: 2021-01-04 18:58:12
+# @Last Modified by: Yeshwanth
+# @Last Modified time: 2021-01-09 12:30:53
+# @Title: Getting group id from machine
+
+import os
+print("Effective group id: ",os.getegid())
+print("Effective user id: ",os.geteuid())
+print("Real group id: ",os.getgid())
+print("List of supplemental group ids: ",os.getgroups())
