38ba74a1e7ceece78a03254e62b8fe15a0712618
yeshwanth281297 <yesh281297@gmail.com>
2021-03-06 08:37:54 +0530

    ADDED : Group Member

---
+++ Week2_basic/Group_member.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Author: Yeshwanth
+# @Date: 2021-01-04 18:58:12
+# @Last Modified by: Yeshwanth
+# @Last Modified time: 2021-01-09 12:30:53
+# @Title: Group_member
+
+def is_group_member(group_data, n):
+    for value in group_data:
+        if n == value:
+            return True
+    return False
+print(is_group_member([1, 5, 8, 3], 3))
+print(is_group_member([5, 8, 3], -1))
