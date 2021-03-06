0c75e9a626786235d2fe68b0fa010f7409a418a0
yeshwanth281297 <yesh281297@gmail.com>
2021-03-06 08:34:36 +0530

    ADDED: Clear Screen

---
+++ Week2_basic/Clear_screen.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author: Yeshwanth
+# @Date: 2021-01-04 18:58:12
+# @Last Modified by: Yeshwanth
+# @Last Modified time: 2021-01-09 12:30:53
+# @Title: Clear Screen
+
+import os
+import time
+os.system("ls")
+time.sleep(10)
+os.system('clear')
