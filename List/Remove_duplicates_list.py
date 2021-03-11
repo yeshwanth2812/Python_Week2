f03d13fc723b14c16efb9e4691521f79cdc3a75a
yeshwanth281297 <yesh281297@gmail.com>
2021-03-11 11:20:42 +0530

    ADDED:remove duplicates from list

---
+++ List/Remove_duplicates_list.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Author: Yeshwanth
+# @Date: 2021-01-04 18:58:12
+# @Last Modified by: Yeshwanth
+# @Last Modified time: 2021-01-09 11:59:53
+# @Title:remove duplicates from list
+
+class DuplicateList:
+    def __init__(self):
+        pass
+    def remove_duplicate(self):
+       
+        list1 = [[10, 20, 30], [40], [30, 56, 25], [10, 20], [33], [40]]
+        new_list = []
+        for elements in list1:
+            if elements not in new_list:
+                new_list.append(elements)
+              
+        print("New list without duplicates",new_list)
+
+if __name__ == "__main__":
+    duplicatelist = DuplicateList()
+    duplicatelist.remove_duplicate()      
