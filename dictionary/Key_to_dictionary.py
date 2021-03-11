acab4ac8e9fbb661b32b8758a9da2472792e6a5f
yeshwanth281297 <yesh281297@gmail.com>
2021-03-11 11:09:43 +0530

    ADDED:Key to a dictionary

---
+++ dictionary/Key_to_dictionary.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author: Yeshwanth
+# @Date: 2021-01-04 18:58:12
+# @Last Modified by: Yeshwanth
+# @Last Modified time: 2021-01-09 12:30:53
+# @Title:add key to a dictionary
+
+dictionary = {0:10, 1:20}
+print("OLD dictionary",dictionary)
+dictionary.update({2:30})
+print("Updated dictionary", dictionary)
